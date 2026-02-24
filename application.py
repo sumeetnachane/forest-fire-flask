d7f5bc01a26ff23e762210e365b09f6ccd0b0c7c
sumeetnachane <166376075+sumeetnachane@users.noreply.github.com>
2026-02-24 13:59:28 +0530

    Changes applied

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pf
@@ -35,5 +36,6 @@ def predict_datapoint():
   else:
     return render_template('home.html')   
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
